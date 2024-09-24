4d0a53b6a0655bd672a0096018dd93ffe01babef
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-24 17:14:09 -0300

    update: header css add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,16 @@
 export const Header = () => {
-  return <header className="flex bg-primary-foreground h-52">Header</header>
+  return (
+    <header className="flex bg-primary-foreground">
+      <div className="flex">
+        <h3>Reginaldo Gomes</h3>
+        <nav>
+          <ul className="flex">
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
