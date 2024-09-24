6bdfef642502fa55915d33e3efb7b9c43ecf23cb
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-23 23:49:31 -0300

    update: hero component add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-import { Button } from '@/components/ui/button'
 import { Main } from '@/components/Main'
+import { Hero } from '@/components/Hero'
 
 export default function Home() {
   return (
     <Main>
-      <Button>Click me</Button>
+      <Hero />
     </Main>
   )
 }
