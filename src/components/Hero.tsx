8271c2c8bc2272f4ce2ff1051341fad6b1b5e703
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-23 23:47:40 -0300

    new: hero component create

---
+++ src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import { Button } from './ui/button'
+
+export const Hero = () => {
+  return (
+    <section>
+      <h1>Desenvolvedor Front-end</h1>
+      <h3>
+        Desenvolvedor Front-End com sólida experiência em JavaScript,
+        especializado em React.js e Next.js para o desenvolvimento de interfaces
+        modernas, dinâmicas e escaláveis. Atuo de forma consistente em ambientes
+        de cloud computing, com foco em AWS, e aplico boas práticas de DevOps
+        para automação e gerenciamento eficiente de infraestrutura. Além disso,
+        tenho conhecimentos em cibersegurança, implementando medidas preventivas
+        e corretivas ao longo de todo o ciclo de desenvolvimento, garantindo a
+        segurança de dados e a integridade das aplicações. Comprometido em
+        entregar soluções inovadoras e robustas, sempre alinhado às melhores
+        práticas do mercado.
+      </h3>
+      <Button variant="ghost">Click me</Button>
+    </section>
+  )
+}
