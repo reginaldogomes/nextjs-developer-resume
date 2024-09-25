7f06b128edca56e504910735ac307d7b2ff97e7c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-25 00:50:04 -0300

    update: center header content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Navbar from './Navbar'
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 z-1 p-2	flex bg-primary-foreground">
-      <div className="flex justify-between w-full items-center">
+    <header className="sticky top-0 z-1 p-2	flex justify-center bg-primary-foreground">
+      <div className="flex justify-between w-full items-center lg:w-[1024px]">
         <h3 className="text-2xl font-semibold">Reginaldo Gomes</h3>
         <Navbar />
       </div>
