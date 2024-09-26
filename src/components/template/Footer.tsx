6906da9bcf4ebb07bea6165b9a59c67ecda07360
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-26 11:22:26 -0300

    update: folder template create

---
+++ src/components/template/Footer.tsx
@@ -0,0 +1,15 @@
+import SocialLinks from '@/components/SocialLinks'
+
+export const Footer = () => {
+  const currentYear = new Date().getFullYear()
+  return (
+    <footer className="flex flex-col justify-center items-center bg-primary-foreground p-16">
+      <div className="flex flex-col justify-center w-full items-center lg:w-[1024px] gap-8">
+        <SocialLinks />
+        <p className="text-center">
+          © {currentYear} Reginaldo Gomes. Todos os direitos reservados
+        </p>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/template/Header.tsx
@@ -0,0 +1,16 @@
+'use client'
+import Navbar from './Navbar'
+import { useProfile } from '@/context/resumeContext'
+
+export const Header = () => {
+  const { profile } = useProfile()
+
+  return (
+    <header className="sticky top-0 z-1 p-4	flex justify-center bg-primary-foreground">
+      <div className="flex justify-between w-full items-center lg:w-[1024px]">
+        <h3 className="text-2xl font-semibold">{profile.name}</h3>
+        <Navbar />
+      </div>
+    </header>
+  )
+}
