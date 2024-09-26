c93632715904ce01c03f513dc14cb8df58f581fa
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-25 21:52:23 -0300

    update: GA analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 'use client'
-import Script from 'next/script'
-import { useEffect } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
-import { pageview, GA_TRACKING_ID } from '@/lib/analitycs'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 // import type { Metadata } from 'next'
 import './globals.css'
 import { Header } from '@/components/Header'
@@ -14,40 +11,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  useEffect(() => {
-    if (GA_TRACKING_ID) {
-      const url = `${pathname}${searchParams.toString()}`
-      pageview(url) // Envia o pageview ao GA sempre que a rota mudar
-    }
-  }, [pathname, searchParams])
   return (
     <html lang="en">
-      <head>
-        {/* Script do Google Analytics */}
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </head>
+      <head>{/* Script do Google Analytics */}</head>
       <body className="flex-col antialiased">
         <ProfileProvider>
           <Header />
           {children}
           <Footer />
         </ProfileProvider>
+        <GoogleAnalytics />
       </body>
     </html>
   )
