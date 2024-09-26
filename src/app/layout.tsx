a1637126fca93907a1715375e7acbb8a47c952a7
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-26 11:20:16 -0300

    new: metadata create

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-'use client'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import './globals.css'
-import { Header } from '@/components/Header'
-import { Footer } from '@/components/Footer'
+import { Header } from '@/components/template/Header'
+import { Footer } from '@/components/template/Footer'
 import { ProfileProvider } from '@/context/resumeContext'
+
+export const metadata: Metadata = {
+  title: 'Reginaldo Gomes - Analista Desenvolvedor Front-end',
+  description:
+    'Conheça Reginaldo Gomes, um analista desenvolvedor front-end especializado em criar soluções digitais eficientes e escaláveis. Com experiência em JavaScript, TypeScript e frameworks modernos como React e Next.js, ele oferece expertise em desenvolvimento web, arquitetura limpa e boas práticas de programação.',
+}
 
 export default function RootLayout({
   children,
