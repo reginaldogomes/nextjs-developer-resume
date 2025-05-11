38489498142db1993109249734ef58c914b21c9d
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2025-05-11 20:08:31 -0300

    feat: add ecosystem configuration for production environment

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'reginaldogomes.dev.br',
+      cwd: '/home/ubuntu/apps/reginaldogomes.dev.br',
+      script: 'npm',
+      args: 'start',
+      interpreter: '/bin/bash',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3003,
+      },
+    },
+  ],
+}
+//
