6274ed4bd2987c7243f1078c5a8cd9aecb49e22f
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2025-05-11 20:47:13 -0300

    fix: update ecosystem configuration to use correct script and port settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,12 @@ module.exports = {
     {
       name: 'reginaldogomes.dev.br',
       cwd: '/home/ubuntu/apps/reginaldogomes.dev.br',
-      script: 'npm',
-      args: 'start',
-      interpreter: '/bin/bash',
+      script: './node_modules/next/dist/bin/next', // 1️⃣ Caminho direto
+      args: 'start', // 2️⃣ Sem porta aqui (usamos no env)
+      interpreter: 'node', // 3️⃣ Usa Node.js normalmente
       env: {
         NODE_ENV: 'production',
-        PORT: 3003,
+        PORT: 3000, // 4️⃣ Porta definida aqui
       },
     },
   ],
