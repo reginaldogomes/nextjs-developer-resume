fd27926a8d2126c9962322fb85c7f379cc154b1b
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2025-05-11 23:39:06 -0300

    fix: enhance Main component to accept className prop and update PageHead for metadata generation

--- src/components/layout/PageHead.tsx
+++ src/components/layout/PageHead.tsx
@@ -1,18 +1,25 @@
 import Breadcrumb from './Breadcrumb'
+import { Metadata } from 'next'
 
 interface PageHeadProps {
   title: string
   description?: string
 }
 
-const PageHead: React.FC<PageHeadProps> = ({ title, description }) => (
-  <div className="w-full p-8 text-center shadow-md bg-accent/5 text-white">
-    {/* Título */}
-    <h1 className="text-3xl font-bold">{title}</h1>
+export const generateMetadata = ({
+  title,
+  description,
+}: PageHeadProps): Metadata => ({
+  title,
+  description,
+})
 
-    {/* Descrição */}
+const PageHead: React.FC<PageHeadProps> = ({ title, description }) => (
+  <header className="w-full p-8 text-center shadow-md bg-accent/5 text-white">
+    <Breadcrumb />
+    <h1 className="text-3xl font-bold">{title}</h1>
     {description && <p className="mt-2 text-lg">{description}</p>}
-  </div>
+  </header>
 )
 
 export default PageHead
