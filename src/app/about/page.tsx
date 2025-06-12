53a25e94713027d76ee0240e476b217c53ecda59
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2025-06-12 20:38:49 -0300

    feat: update About section formatting for improved readability and add whitespace handling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,8 @@ import PageHead from '@/components/layout/PageHead'
 
 const userData = {
   about:
-    'Sou natural de Pirapora, Minas Gerais, e atualmente resido em Belo Horizonte. Tenho 45 anos e atuo na área de tecnologia há mais de duas décadas, com forte especialização em design de interfaces e desenvolvimento web. Atualmente, estou em processo de graduação em Gestão da Tecnologia da Informação, ampliando ainda mais minha visão estratégica sobre soluções digitais e infraestrutura tecnológica. Sou apaixonado por tecnologia, natureza e música. Nos meus momentos livres, gosto de viajar para conhecer novos lugares, explorar diferentes culturas e praticar esportes ao ar livre. Acredito que essas vivências fortalecem minha criatividade e me ajudam a manter o equilíbrio entre a vida pessoal e profissional.',
+    'Sou natural de Pirapora, Minas Gerais, e atualmente resido em Belo Horizonte. Tenho 45 anos e atuo na área de tecnologia há mais de duas décadas, com forte especialização em design de interfaces e desenvolvimento web. Atualmente, estou em processo de graduação em Gestão da Tecnologia da Informação, ampliando ainda mais minha visão estratégica sobre soluções digitais e infraestrutura tecnológica.\n\nSou apaixonado por tecnologia, natureza e música. Nos meus momentos livres, gosto de viajar para conhecer novos lugares, explorar diferentes culturas e praticar esportes ao ar livre. Acredito que essas vivências fortalecem minha criatividade e me ajudam a manter o equilíbrio entre a vida pessoal e profissional.',
+
   journey:
     'Minha carreira começou em Brasília, no início dos anos 2000, quando tive a oportunidade de atuar como web designer. Essa experiência marcou o início da minha jornada no desenvolvimento web, área pela qual me apaixonei e sigo até hoje. Desde então, passei por empresas de diferentes segmentos e regiões do Brasil, incluindo capitais como São Paulo, Florianópolis e Belo Horizonte. Atuei em projetos relevantes em empresas como Softplan, Saraiva, Locaweb e Squadra, construindo uma base sólida em tecnologia e design digital.',
 }
@@ -42,7 +43,9 @@ export default function Page() {
               <h2 className="text-2xl font-semibold text-primary mb-4">
                 {section.title}
               </h2>
-              <p className="text-muted leading-relaxed">{section.content}</p>
+              <p className="text-muted leading-relaxed whitespace-pre-line text-justify">
+                {section.content}
+              </p>
             </motion.section>
           ))}
         </motion.div>
